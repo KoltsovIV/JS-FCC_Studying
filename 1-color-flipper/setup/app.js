5e83c38d5d73a334fe48f88b27db0942be05539b
KoltsovIV <koltsov273.15@gmail.com>
2021-09-20 20:32:21 +0300

    made random color switcher for background page

--- 1-color-flipper/setup/app.js
+++ 1-color-flipper/setup/app.js
@@ -1,1 +1,18 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
+const btn = document.querySelector('.btn-hero');
+const color = document.querySelector('.color');
+
+
+btn.addEventListener('click', () => {
+
+    let num = randomNumber(colors);
+
+    document.body.style.background = colors[num];
+    color.textContent = colors[num];
+
+}); 
+
+function randomNumber (array) {
+    return Math.floor(Math.random()*array.length);
+}
--- 1-color-flipper/setup/hex.js
+++ 1-color-flipper/setup/hex.js
@@ -1,1 +1,22 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+const btn = document.querySelector('.btn-hero');
+const color = document.querySelector('.color');
+
+
+btn.addEventListener('click', () => {
+    
+    let hexColor = getHexColor (hex);
+
+    document.body.style.background = hexColor;
+    color.textContent = hexColor;
+
+}); 
+
+function getHexColor (array) {
+    let colorHex = ['#'];
+    for (let i = 0; i < 6; i++){
+        colorHex.push(array[Math.floor(Math.random()*array.length)]);
+    }
+    return colorHex.join('');
+}
