a0fb6c667cd355d82feeee9bea4a8713c13f2521
KoltsovIV <koltsov273.15@gmail.com>
2021-09-25 23:26:01 +0300

    added two different solutions: personal and lecturer

--- 11-tabs/setup/app.js
+++ 11-tabs/setup/app.js
@@ -1,31 +1,25 @@
 const btnTab = document.querySelectorAll('.tab-btn'),
-      content = document.querySelectorAll('.content');
+      content = document.querySelectorAll('.content'),
+      sectionCenter = document.querySelector('.about-center');
 
-// add first elements of btn and content container after DOM loaded
-window.addEventListener('DOMContentLoaded', () => {
-    btnTab[0].classList.add('active');
-    content[0].classList.add('active');
-});
+sectionCenter.addEventListener('click', (event) => {
 
-btnTab.forEach((item) => {
-    item.addEventListener('click', () => {
+    const index = event.target.dataset.id;
 
-        // delete from all btns class "active" after click on any btn
-        // after what - add class "active" to btn that was used
-        btnTab.forEach((element) => {
-            element.classList.remove('active');
+    if (index) {
+        btnTab.forEach((item) => {
+            item.classList.remove('active');
         });
-        item.classList.add('active');
+        content.forEach((item) => {
+            item.classList.remove('active');
+        });
 
-        // delete from all content containers class "active" after click on any btn
-        // after what - add class "active" to content container that conected with
-        // used btn by data-id
-        content.forEach((element) => {
-            element.classList.remove('active');
-            const idIndex = item.dataset.id;            
-            if  (element.id == idIndex) {
-                element.classList.add('active');
+        event.target.classList.add('active');
+        content.forEach((item) => {
+            if(item.id == index) {
+                item.classList.add('active');
             }
         });
-    });
+        
+    }
 });
